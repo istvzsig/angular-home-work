9c580e919ace3e90602e5378ca30243e074cb864
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-18 22:14:43 +0200

    👽 set up routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { InfinitePhotoStreamComponent } from './components/infinite-photo-stream/infinite-photo-stream.component';
+import { FavoritePhotosComponent } from './components/favorite-photos/favorite-photos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: InfinitePhotoStreamComponent
+  },
+  {
+    path: 'favorites',
+    component: FavoritePhotosComponent
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
