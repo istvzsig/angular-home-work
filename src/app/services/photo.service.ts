809f2380645ab658023ef60f4e774b181a82d3a8
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-19 18:46:38 +0200

    🔧 UPDATE PhotoService

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -5,9 +5,8 @@ import { Photo } from '../models/photo';
 import { FavoritePhotosComponent } from '../components/favorite-photos/favorite-photos.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class PhotoService {
   public imagesLoaded: boolean = false;
   public favorites: Photo[] = [];
@@ -17,23 +16,29 @@ export class PhotoService {
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
       'Autohorization': '',
-  })};
+    }),
+  };
 
   readonly url: string = `https://picsum.photos/v2/list?page=2&limit=${this.batchLimit}`;
-  
+
   constructor(private http: HttpClient) {}
 
   public getPhotos(): Observable<Photo[]> {
-    return this.http.get<Photo[]>(this.url)
+    return this.http.get<Photo[]>(this.url);
   }
-  
+
   public addToFavoritePhoto(photoId: string) {
-    console.log('post favorite photo');
-    return this.http.post('http://120.0.0.1:4200/photos', {id: photoId}, this._options)
+    return this.http.post(
+      'http://120.0.0.1:4200/photos',
+      { id: photoId },
+      this._options
+    );
   }
 
   public removeFavoritePhoto(photoId: string) {
-    console.log('delete favorite photo');
-    return this.http.delete(`http://120.0.0.1:4200/photos/${photoId}`, this._options);
+    return this.http.delete(
+      `http://120.0.0.1:4200/photos/${photoId}`,
+      this._options
+    );
   }
 }
