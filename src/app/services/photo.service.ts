2a2e545ad4431c529adf9dc01efe4bc1792027b6
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-19 21:52:46 +0200

    🔧 UPDATE PhotoService

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, delay, of } from 'rxjs';
 import { Photo } from '../models/photo';
 import { FavoritePhotosComponent } from '../components/favorite-photos/favorite-photos.component';
 
@@ -8,10 +8,13 @@ import { FavoritePhotosComponent } from '../components/favorite-photos/favorite-
   providedIn: 'root',
 })
 export class PhotoService {
-  public imagesLoaded: boolean = false;
+  public photos: Photo[];
   public favorites: Photo[] = [];
-  private batchLimit = 20;
-  private _options = {
+
+  public page: number = 1;
+  public batchLimit: number = 12;
+
+  private headerConfig = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
       'Access-Control-Allow-Origin': '*',
@@ -19,26 +22,28 @@ export class PhotoService {
     }),
   };
 
-  readonly url: string = `https://picsum.photos/v2/list?page=2&limit=${this.batchLimit}`;
-
   constructor(private http: HttpClient) {}
 
-  public getPhotos(): Observable<Photo[]> {
-    return this.http.get<Photo[]>(this.url);
+  public getPhotos(page: number = 1, batchLimit: number = 1): Observable<Photo[]> {
+    return this.http.get<Photo[]>(`https://picsum.photos/v2/list?page=${page}&limit=${batchLimit}`)
   }
 
   public addToFavoritePhoto(photoId: string) {
     return this.http.post(
       'http://120.0.0.1:4200/photos',
       { id: photoId },
-      this._options
+      this.headerConfig
     );
   }
 
   public removeFavoritePhoto(photoId: string) {
     return this.http.delete(
       `http://120.0.0.1:4200/photos/${photoId}`,
-      this._options
+      this.headerConfig
     );
+  }
+
+  updateUrl(url: string): string {
+    return 'asd'
   }
 }
