cafe8a532dbf715c235fc8e53c993ca865069955
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-19 18:44:05 +0200

    🧪 TEST PhotoService

--- src/app/services/photo.service.spec.ts
+++ src/app/services/photo.service.spec.ts
@@ -3,46 +3,95 @@ import { TestBed } from '@angular/core/testing';
 import { PhotoService } from './photo.service';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { InfinitePhotoStreamComponent } from '../components/infinite-photo-stream/infinite-photo-stream.component';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { Photo } from '../models/photo';
+import { Observable } from 'rxjs';
 
 describe('PhotoService', () => {
+  let httpMock: HttpTestingController;
   let photoService: PhotoService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      imports: [HttpClientModule],
-      declarations: [InfinitePhotoStreamComponent],
-      providers: [HttpClient, PhotoService]
-    });
-    photoService = TestBed.inject(PhotoService);
-  });
-
-  it('should create', () => {
-    expect(photoService).toBeTruthy();
-  });
-});
-
-describe('HttpTesting', () => {
-  let photoService: PhotoService;
-  let httpClient: HttpClient;
-  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      declarations: [InfinitePhotoStreamComponent],
-      providers: [HttpClient, PhotoService]
+      providers: [PhotoService],
     });
 
-    httpClient = TestBed.inject(HttpClient);
-    httpTestingController = TestBed.inject(HttpTestingController);
     photoService = TestBed.inject(PhotoService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
-  it('should test HttpClient.get', () => {
-    httpClient.get(photoService.url).subscribe((data) => {
-      expect(typeof data).toBe('object')
+  it('getPhotos() should http GET names', () => {
+    const names: Photo[] = [
+      {
+        id: 0,
+        author: 'asdsad',
+        width: 2,
+        height: 2,
+        url: 'string',
+        download_url: 'string',
+        isFavorite: false,
+      },
+    ];
+
+    photoService.getPhotos().subscribe((res) => {
+      expect(res).toEqual(names);
     });
+
+    const request = httpMock.expectOne(
+      'https://picsum.photos/v2/list?page=2&limit=20'
+    );
+    expect(request.request.method).toEqual('GET');
+    request.flush(names);
+
+    httpMock.verify();
+  });
+
+  it('addToFavoritePhoto() should add photo from favorites', () => {
+    const names: Photo[] = [
+      {
+        id: 0,
+        author: 'asdsad',
+        width: 2,
+        height: 2,
+        url: 'string',
+        download_url: 'string',
+        isFavorite: false,
+      },
+    ];
+    photoService.addToFavoritePhoto('0').subscribe((res) => {
+      expect(res).toEqual(names);
+    });
+
+    const request = httpMock.expectOne('http://120.0.0.1:4200/photos');
+    expect(request.request.method).toEqual('POST');
+    request.flush(names);
+
+    httpMock.verify();
+  });
+
+  it('removeFavoritePhoto() should remove photo from favorites', () => {
+    const names: Photo[] = [
+      {
+        id: 0,
+        author: 'asdsad',
+        width: 2,
+        height: 2,
+        url: 'string',
+        download_url: 'string',
+        isFavorite: false,
+      },
+    ];
+    photoService.removeFavoritePhoto('0').subscribe((res) => {
+      expect(res).toEqual(names);
+    });
+
+    const request = httpMock.expectOne('http://120.0.0.1:4200/photos/0');
+    expect(request.request.method).toEqual('DELETE');
+    request.flush(names);
+    httpMock.verify();
   });
 });
