6a962f4aa5fdb5a3224e1c0e65c8f9fecb3b3d87
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-19 07:46:13 +0200

    🧪 TEST - TopNavigationComponent

--- src/app/components/top-navigation/top-navigation.component.spec.ts
+++ src/app/components/top-navigation/top-navigation.component.spec.ts
@@ -1,13 +1,19 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, fakeAsync, tick, waitForAsync } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { TopNavigationComponent } from './top-navigation.component';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
+import { InfinitePhotoStreamComponent } from '../infinite-photo-stream/infinite-photo-stream.component';
+import { FavoritePhotosComponent } from '../favorite-photos/favorite-photos.component';
+import { routes } from 'src/app/app-routing.module';
 
 describe('TopNavigationComponent', () => {
   let component: TopNavigationComponent;
   let fixture: ComponentFixture<TopNavigationComponent>;
-
+  
   beforeEach(() => {
     TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [TopNavigationComponent]
     });
     fixture = TestBed.createComponent(TopNavigationComponent);
@@ -24,4 +30,43 @@ describe('TopNavigationComponent', () => {
     const numberOfLinks = compiled.querySelectorAll('.top-navigation--link').length;
     expect(numberOfLinks).toEqual(2);
   });
+});
+
+describe('RouterTest', () => {
+  let router: Router;
+  let fixture: ComponentFixture<TopNavigationComponent>;
+  let component: TopNavigationComponent;
+  let location: Location;
+
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      imports: [RouterTestingModule.withRoutes(routes)],
+      declarations: [TopNavigationComponent, InfinitePhotoStreamComponent, FavoritePhotosComponent
+      ]}).compileComponents().then(() => {
+        router = TestBed.inject(Router);
+        spyOn(router, 'navigate').and.returnValue(Promise.resolve(true));
+      });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TopNavigationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    router.initialNavigation();
+  });
+
+  it(`should go to path '/'`, waitForAsync(() => {
+    component.handleClick('');
+    expect(router.navigate).toHaveBeenCalledWith(['/']);
+  }));
+
+  it(`should go to path '/favorites'`, waitForAsync(() => {
+    component.handleClick('favorites');
+    expect(router.navigate).toHaveBeenCalledWith(['/favorites']);
+  }));
+
+  it(`should go to path '/**'`, waitForAsync(() => {
+    component.handleClick('**');
+    expect(router.navigate).toHaveBeenCalledWith(['/**']);
+  }));
 });
