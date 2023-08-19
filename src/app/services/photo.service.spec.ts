34ba67ac7455509422a8698e05b687be1483838a
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-19 18:04:47 +0200

    🔧 UPDATE PhotoService

--- src/app/services/photo.service.spec.ts
+++ src/app/services/photo.service.spec.ts
@@ -1,16 +1,48 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PhotoService } from './photo.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { InfinitePhotoStreamComponent } from '../components/infinite-photo-stream/infinite-photo-stream.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { Photo } from '../models/photo';
 
 describe('PhotoService', () => {
-  let service: PhotoService;
+  let photoService: PhotoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PhotoService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+      declarations: [InfinitePhotoStreamComponent],
+      providers: [HttpClient, PhotoService]
+    });
+    photoService = TestBed.inject(PhotoService);
   });
 
   it('should create', () => {
-    expect(service).toBeTruthy();
+    expect(photoService).toBeTruthy();
+  });
+});
+
+describe('HttpTesting', () => {
+  let photoService: PhotoService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      declarations: [InfinitePhotoStreamComponent],
+      providers: [HttpClient, PhotoService]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    photoService = TestBed.inject(PhotoService);
+  });
+
+  it('should test HttpClient.get', () => {
+    httpClient.get(photoService.url).subscribe((data) => {
+      expect(typeof data).toBe('object')
+    });
   });
 });
--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,19 +1,39 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Photo } from '../photo';
+import { Photo } from '../models/photo';
+import { FavoritePhotosComponent } from '../components/favorite-photos/favorite-photos.component';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class PhotoService {
-  public photos: Observable<Photo[]>;
-  readonly url: string = 'https://picsum.photos/v2/list';
+  public imagesLoaded: boolean = false;
+  public favorites: Photo[] = [];
+  private batchLimit = 20;
+  private _options = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Autohorization': '',
+  })};
+
+  readonly url: string = `https://picsum.photos/v2/list?page=2&limit=${this.batchLimit}`;
   
   constructor(private http: HttpClient) {}
 
-  public getPhotos(url: string = this.url): any {
-    return this.http.get<Photo[]>(url || this.url)
+  public getPhotos(): Observable<Photo[]> {
+    return this.http.get<Photo[]>(this.url)
+  }
+  
+  public addToFavoritePhoto(photoId: string) {
+    console.log('post favorite photo');
+    return this.http.post('http://120.0.0.1:4200/photos', {id: photoId}, this._options)
   }
 
+  public removeFavoritePhoto(photoId: string) {
+    console.log('delete favorite photo');
+    return this.http.delete(`http://120.0.0.1:4200/photos/${photoId}`, this._options);
+  }
 }
