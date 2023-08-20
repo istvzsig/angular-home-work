6edd290fb0e368189033f448fd5e6b44a1f13b4d
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-20 04:21:17 +0200

    👽 UPDATE routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InfinitePhotoStreamComponent } from './components/infinite-photo-stream/infinite-photo-stream.component';
 import { FavoritePhotosComponent } from './components/favorite-photos/favorite-photos.component';
+import { PhotoPageComponent } from './components/photo-page/photo-page.component';
 
 export const routes: Routes = [
   { path: '', component: InfinitePhotoStreamComponent, pathMatch: 'full' },
   { path: 'favorites', component: FavoritePhotosComponent, pathMatch: 'full' },
-  { path: 'photos', redirectTo: '', pathMatch: 'full' },
+  { path: 'photos/:id', component: PhotoPageComponent, pathMatch: 'full'},
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
 
