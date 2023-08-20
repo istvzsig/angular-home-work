c86b396ce75c9a9041f14b1b3cf65864d3451dab
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-20 17:50:27 +0200

    🔧 UPDATE infinite scroll service

--- src/app/services/infinite-scroll/infinite-scroll.service.ts
+++ src/app/services/infinite-scroll/infinite-scroll.service.ts
@@ -2,25 +2,23 @@ import { Injectable } from '@angular/core';
 import { PhotoService } from '../photo-service/photo.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class InfiniteScrollService {
   public isLoading: boolean = false;
-  public delay = Math.floor(Math.random() * (300 - 200 + 1) + 200);
-
-  constructor() {}
 
   public updateOnScrollEvent(photoService: PhotoService): void {
-    window.addEventListener('scroll',  (e:Event) => {
-      const { scrollTop, clientHeight, scrollHeight } = document.documentElement;
-      if(scrollTop +  clientHeight + 1 > scrollHeight) {
-       photoService.pageNumber++;
-       photoService.appendPhotos();
+    window.addEventListener('scroll', (e: Event) => {
+      const { scrollTop, clientHeight, scrollHeight } =
+        document.documentElement;
+      if (scrollTop + clientHeight >= scrollHeight) {
+        photoService.pageNumber++;
+        photoService.appendPhotos();
       }
     });
   }
 
   public toggleLoading(): boolean {
-    return this.isLoading = !this.isLoading;
+    return (this.isLoading = !this.isLoading);
   }
 }
