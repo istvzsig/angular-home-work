e21ef8c7d0d198e240e9484a51c01846c8fad0d0
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-20 23:17:20 +0200

    📦 add environment variables

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+  baseApiUrl: 'https://picsum.photos',
+  localHostUrl: '127.0.0.1:4200',
+  photoProcess: {
+    pageNumber: 1,
+    batchSize: 12,
+    randomDelay: Math.floor(Math.random() * (3000 - 1200 + 1) + 1200),
+  },
+};
