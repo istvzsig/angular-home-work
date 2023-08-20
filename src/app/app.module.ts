bbefc8065ad496a6ee2c5cf7e40f40b9739ace00
István Zsigmond <istvan@Istvans-MacBook-Pro.local>
2023-08-20 18:46:07 +0200

    🔧 UPDATE import in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { PhotoService } from 'src/app/services/photo-service/photo.service';
-import { InfiniteScrollService } from './services/infinite-scroll/infinite-scroll.service';
+import { InfiniteScrollService } from './services/infinite-scroll-service/infinite-scroll.service';
 
 import { AppComponent } from './app.component';
 import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { PhotoStreamComponent } from './components/photo-stream/photo-stream-component';
 import { FavoritePhotosComponent } from './components/favorite-photos/favorite-photos.component';
+import { PhotoStreamComponent } from './components/photo-stream/photo-stream-component';
 
 @NgModule({
   declarations: [
